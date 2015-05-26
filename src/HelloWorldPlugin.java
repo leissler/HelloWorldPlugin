095dc00804b727abec38c1c8aa1fcb1e308012ec
Martin Leissler <martin@leissler.net>
2015-05-26 11:19:55 +0200

    Kill player command added

--- src/HelloWorldPlugin.java
+++ src/HelloWorldPlugin.java
@@ -4,6 +4,11 @@
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HelloWorldPlugin extends JavaPlugin {
@@ -21,5 +26,48 @@ public class HelloWorldPlugin extends JavaPlugin {
         log.info("[HelloWorldPlugin] Stopping...");
     }
 
+    public void onPlayerInteractBlock(PlayerInteractEvent event){
 
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("hello")) {
+
+            if(sender instanceof Player) {
+                String message = "[Server] Hey, here is the Hello World Plugin, dude!";
+                getServer().broadcastMessage(message);
+
+                return true;
+            } else {
+                sender.sendMessage("Du Hirsch, der Command geht nur als Player");
+                return true;
+            }
+
+        } else if (command.getName().equalsIgnoreCase("killplayer")) { // Check if the command is "killplayer"
+            // Get the player in a variable (from args[0])
+
+            Player opfer = sender.getServer().getPlayer(args[0]);
+
+            if(args.length != 1) {
+                sender.sendMessage("Please specify a player to kill.");
+                return true;
+            }
+
+            // Make sure the player is online (is it null?)
+
+            if(opfer == null) {
+                sender.sendMessage("Player "+args[0]+" is not online!");
+                return true;
+            }
+
+            // if player is online -> setHealth(0.0D)
+            opfer.setHealth(0.0);
+
+        }
+
+        return true;
+
+
+    }
 }
